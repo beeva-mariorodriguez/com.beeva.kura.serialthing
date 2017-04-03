0567ec98a5cb3a5ae4e984df4af2bf00f69a0a37
Mario Rodríguez <mario.rodriguez@beeva.com>
2017-04-03 12:33:17 +0200

    flow control configuration support

--- src/com/beeva/kura/serialthing/SerialThing.java
+++ src/com/beeva/kura/serialthing/SerialThing.java
@@ -1,3 +1,4 @@
+
 package com.beeva.kura.serialthing;
 
 import java.io.IOException;
@@ -18,229 +19,235 @@ import org.slf4j.LoggerFactory;
 
 public class SerialThing implements ConfigurableComponent {
 
-    private static final Logger s_logger = LoggerFactory.getLogger(SerialThing.class);
-    private static final String APP_ID = "com.beeva.kura.serialthing.SerialThing";
+	private static final Logger s_logger = LoggerFactory.getLogger(SerialThing.class);
+	private static final String APP_ID = "com.beeva.kura.serialthing.SerialThing";
 
-    private static final String SERIAL_DEVICE_PROP_NAME = "serial.device";
-    private static final String SERIAL_BAUDRATE_PROP_NAME = "serial.baudrate";
-    private static final String SERIAL_DATA_BITS_PROP_NAME = "serial.data-bits";
-    private static final String SERIAL_PARITY_PROP_NAME = "serial.parity";
-    private static final String SERIAL_STOP_BITS_PROP_NAME = "serial.stop-bits";
+	private static final String SERIAL_DEVICE_PROP_NAME = "serial.device";
+	private static final String SERIAL_BAUDRATE_PROP_NAME = "serial.baudrate";
+	private static final String SERIAL_DATA_BITS_PROP_NAME = "serial.data-bits";
+	private static final String SERIAL_PARITY_PROP_NAME = "serial.parity";
+	private static final String SERIAL_STOP_BITS_PROP_NAME = "serial.stop-bits";
+	private static final String SERIAL_FLOWCONTROL_PROP_NAME = "serial.flowcontrol";
 
-    private ConnectionFactory m_connectionFactory;
-    private CommConnection m_commConnection;
-    private InputStream m_commIs;
-    private OutputStream m_commOs;
+	private ConnectionFactory m_connectionFactory;
+	private CommConnection m_commConnection;
+	private InputStream m_commIs;
+	private OutputStream m_commOs;
 
-    private ScheduledThreadPoolExecutor m_worker;
-    private Future<?> m_handle;
+	private ScheduledThreadPoolExecutor m_worker;
+	private Future<?> m_handle;
 
-    private Map<String, Object> m_properties;
+	private Map<String, Object> m_properties;
 
-    // ----------------------------------------------------------------
-    //
-    // Dependencies
-    //
-    // ----------------------------------------------------------------
+	// ----------------------------------------------------------------
+	//
+	// Dependencies
+	//
+	// ----------------------------------------------------------------
 
-    public void setConnectionFactory(ConnectionFactory connectionFactory) {
-        this.m_connectionFactory = connectionFactory;
-    }
+	public void setConnectionFactory(ConnectionFactory connectionFactory) {
+		this.m_connectionFactory = connectionFactory;
+	}
 
-    public void unsetConnectionFactory(ConnectionFactory connectionFactory) {
-        this.m_connectionFactory = null;
-    }
+	public void unsetConnectionFactory(ConnectionFactory connectionFactory) {
+		this.m_connectionFactory = null;
+	}
 
-    // ----------------------------------------------------------------
-    //
-    // Activation APIs
-    //
-    // ----------------------------------------------------------------
+	// ----------------------------------------------------------------
+	//
+	// Activation APIs
+	//
+	// ----------------------------------------------------------------
 
-    protected void activate(ComponentContext componentContext, Map<String, Object> properties) {
-        s_logger.info("Activating " + APP_ID);
-        m_worker = new ScheduledThreadPoolExecutor(1);
-        m_properties = new HashMap<String, Object>();
-        doUpdate(properties);
-        s_logger.info("Activating " + APP_ID + "Done.");
-    }
+	protected void activate(ComponentContext componentContext, Map<String, Object> properties) {
+		s_logger.info("Activating " + APP_ID);
+		m_worker = new ScheduledThreadPoolExecutor(1);
+		m_properties = new HashMap<String, Object>();
+		doUpdate(properties);
+		s_logger.info("Activating " + APP_ID + "Done.");
+	}
 
-    protected void deactivate(ComponentContext componentContext) {
-        s_logger.info("Deactivating " + APP_ID);
+	protected void deactivate(ComponentContext componentContext) {
+		s_logger.info("Deactivating " + APP_ID);
 
-        // shutting down the worker and cleaning up the properties
-        m_handle.cancel(true);
-        m_worker.shutdownNow();
+		// shutting down the worker and cleaning up the properties
+		m_handle.cancel(true);
+		m_worker.shutdownNow();
 
-        // close the serial port
-        closePort();
-        s_logger.info("Deactivating  " + APP_ID + "Done.");
+		// close the serial port
+		closePort();
+		s_logger.info("Deactivating  " + APP_ID + "Done.");
 
-    }
+	}
 
-    public void updated(Map<String, Object> properties) {
-        s_logger.info("Updating " + APP_ID);
-        doUpdate(properties);
-        s_logger.info("Updating " + APP_ID);
-    }
+	public void updated(Map<String, Object> properties) {
+		s_logger.info("Updating " + APP_ID);
+		doUpdate(properties);
+		s_logger.info("Updating " + APP_ID);
+	}
 
-    // ----------------------------------------------------------------
-    //
-    // Private Methods
-    //
-    // ----------------------------------------------------------------
+	// ----------------------------------------------------------------
+	//
+	// Private Methods
+	//
+	// ----------------------------------------------------------------
 
-    /**
-     * Called after a new set of properties has been configured on the service
-     */
-    private void doUpdate(Map<String, Object> properties) {
-        try {
-            for (String s : properties.keySet()) {
-                s_logger.info("Update - " + s + ": " + properties.get(s));
-            }
+	/**
+	 * Called after a new set of properties has been configured on the service
+	 */
+	private void doUpdate(Map<String, Object> properties) {
+		try {
+			for (String s : properties.keySet()) {
+				s_logger.info("Update - " + s + ": " + properties.get(s));
+			}
 
-            // cancel a current worker handle if one if active
-            if (m_handle != null) {
-                m_handle.cancel(true);
-            }
+			// cancel a current worker handle if one if active
+			if (m_handle != null) {
+				m_handle.cancel(true);
+			}
 
-            // close the serial port so it can be reconfigured
-            closePort();
+			// close the serial port so it can be reconfigured
+			closePort();
 
-            // store the properties
-            m_properties.clear();
-            m_properties.putAll(properties);
+			// store the properties
+			m_properties.clear();
+			m_properties.putAll(properties);
 
-            // reopen the port with the new configuration
-            openPort();
+			// reopen the port with the new configuration
+			openPort();
 
-            // start the worker thread
-            m_handle = m_worker.submit(new Runnable() {
+			// start the worker thread
+			m_handle = m_worker.submit(new Runnable() {
 
-                @Override
-                public void run() {
-                    doSerial();
-                }
-            });
-        } catch (Throwable t) {
-            s_logger.error("Unexpected Throwable", t);
-        }
-    }
+				@Override
+				public void run() {
+					doSerial();
+				}
+			});
+		} catch (Throwable t) {
+			s_logger.error("Unexpected Throwable", t);
+		}
+	}
 
-    private void openPort() {
-        String port = (String) m_properties.get(SERIAL_DEVICE_PROP_NAME);
+	private void openPort() {
+		String port = (String) m_properties.get(SERIAL_DEVICE_PROP_NAME);
 
-        if (port == null) {
-            s_logger.info("Port name not configured");
-            return;
-        }
-        int baudRate = Integer.valueOf((String) m_properties.get(SERIAL_BAUDRATE_PROP_NAME));
-        int dataBits = Integer.valueOf((String) m_properties.get(SERIAL_DATA_BITS_PROP_NAME));
-        int stopBits = Integer.valueOf((String) m_properties.get(SERIAL_STOP_BITS_PROP_NAME));
+		if (port == null) {
+			s_logger.info("Port name not configured");
+			return;
+		}
+		int baudRate = Integer.valueOf((String) m_properties.get(SERIAL_BAUDRATE_PROP_NAME));
+		int dataBits = Integer.valueOf((String) m_properties.get(SERIAL_DATA_BITS_PROP_NAME));
+		int stopBits = Integer.valueOf((String) m_properties.get(SERIAL_STOP_BITS_PROP_NAME));
 
-        String sParity = (String) m_properties.get(SERIAL_PARITY_PROP_NAME);
+		String sParity = (String) m_properties.get(SERIAL_PARITY_PROP_NAME);
 
-        int parity = CommURI.PARITY_NONE;
-        if (sParity.equals("none")) {
-            parity = CommURI.PARITY_NONE;
-        } else if (sParity.equals("odd")) {
-            parity = CommURI.PARITY_ODD;
-        } else if (sParity.equals("even")) {
-            parity = CommURI.PARITY_EVEN;
-        }
+		int parity = CommURI.PARITY_NONE;
+		if (sParity.equals("odd")) {
+			parity = CommURI.PARITY_ODD;
+		} else if (sParity.equals("even")) {
+			parity = CommURI.PARITY_EVEN;
+		}
 
-        String uri = new CommURI.Builder(port).withBaudRate(baudRate).withDataBits(dataBits).withStopBits(stopBits)
-                .withParity(parity).withTimeout(1000).build().toString();
-        try {
-            m_commConnection = (CommConnection) m_connectionFactory.createConnection(uri, 1, false);
-            m_commIs = m_commConnection.openInputStream();
-            m_commOs = m_commConnection.openOutputStream();
-            s_logger.info(port + " open");
-        } catch (IOException e) {
-            s_logger.error("Failed to open port " + port, e);
-            cleanupPort();
-        }
-    }
+		String sFlowcontrol = (String) m_properties.get(SERIAL_FLOWCONTROL_PROP_NAME);
+		int flowcontrol = CommURI.FLOWCONTROL_NONE;
+		if (sFlowcontrol.equals("rtscts")) {
+			flowcontrol = CommURI.FLOWCONTROL_RTSCTS_IN + CommURI.FLOWCONTROL_RTSCTS_OUT;
+		} else if (sFlowcontrol.equals("xonxoff")) {
+			flowcontrol = CommURI.FLOWCONTROL_XONXOFF_IN + CommURI.FLOWCONTROL_XONXOFF_OUT;
+		}
 
-    private void cleanupPort() {
-        if (m_commIs != null) {
-            try {
-                s_logger.info("Closing port input stream...");
-                m_commIs.close();
-                s_logger.info("Closed port input stream");
-            } catch (IOException e) {
-                s_logger.error("Cannot close port input stream", e);
-            }
-            m_commIs = null;
-        }
-        if (m_commOs != null) {
-            try {
-                s_logger.info("Closing port output stream...");
-                m_commOs.close();
-                s_logger.info("Closed port output stream");
-            } catch (IOException e) {
-                s_logger.error("Cannot close port output stream", e);
-            }
-            m_commOs = null;
-        }
-        if (m_commConnection != null) {
-            try {
-                s_logger.info("Closing port...");
-                m_commConnection.close();
-                s_logger.info("Closed port");
-            } catch (IOException e) {
-                s_logger.error("Cannot close port", e);
-            }
-            m_commConnection = null;
-        }
-    }
+		String uri = new CommURI.Builder(port).withBaudRate(baudRate).withDataBits(dataBits).withStopBits(stopBits)
+				.withParity(parity).withTimeout(1000).withFlowControl(flowcontrol).build().toString();
+		try {
+			m_commConnection = (CommConnection) m_connectionFactory.createConnection(uri, 1, false);
+			m_commIs = m_commConnection.openInputStream();
+			m_commOs = m_commConnection.openOutputStream();
+			s_logger.info(port + " open");
+		} catch (IOException e) {
+			s_logger.error("Failed to open port " + port, e);
+			cleanupPort();
+		}
+	}
 
-    private void closePort() {
-        cleanupPort();
-    }
+	private void cleanupPort() {
+		if (m_commIs != null) {
+			try {
+				s_logger.info("Closing port input stream...");
+				m_commIs.close();
+				s_logger.info("Closed port input stream");
+			} catch (IOException e) {
+				s_logger.error("Cannot close port input stream", e);
+			}
+			m_commIs = null;
+		}
+		if (m_commOs != null) {
+			try {
+				s_logger.info("Closing port output stream...");
+				m_commOs.close();
+				s_logger.info("Closed port output stream");
+			} catch (IOException e) {
+				s_logger.error("Cannot close port output stream", e);
+			}
+			m_commOs = null;
+		}
+		if (m_commConnection != null) {
+			try {
+				s_logger.info("Closing port...");
+				m_commConnection.close();
+				s_logger.info("Closed port");
+			} catch (IOException e) {
+				s_logger.error("Cannot close port", e);
+			}
+			m_commConnection = null;
+		}
+	}
 
-    private void doSerial() {
-        if (m_commIs != null) {
-            try {
-                int c = -1;
-                StringBuilder sb = new StringBuilder();
+	private void closePort() {
+		cleanupPort();
+	}
 
-                while (m_commIs != null) {
-                    if (m_commIs.available() != 0) {
-                        c = m_commIs.read();
-                    } else {
-                        try {
-                            Thread.sleep(100);
-                            continue;
-                        } catch (InterruptedException e) {
-                            return;
-                        }
-                    }
+	private void doSerial() {
+		if (m_commIs != null) {
+			try {
+				int c = -1;
+				StringBuilder sb = new StringBuilder();
+				while (m_commIs != null) {
+					if (m_commIs.available() != 0) {
+						c = m_commIs.read();
+					} else {
+						try {
+							Thread.sleep(100);
+							s_logger.debug(APP_ID + "no serial input ... sleeping");
+							continue;
+						} catch (InterruptedException e) {
+							return;
+						}
+					}
+					// on reception of CR, publish the received sentence
+					if (c == 13) {
+						s_logger.debug(APP_ID + "serial input: " + sb.toString());
+						sb.append("\n");
+						String dataRead = sb.toString();
 
-                    // on reception of CR, publish the received sentence
-                    if (c == 13) {
-                        s_logger.debug("Received serial input, echoing to output: " + sb.toString());
-                        sb.append("\r\n");
-                        String dataRead = sb.toString();
+						// echo the data to the output stream
+						m_commOs.write(dataRead.getBytes());
 
-                        // echo the data to the output stream
-                        m_commOs.write(dataRead.getBytes());
-
-                        // reset the buffer
-                        sb = new StringBuilder();
-                    } else if (c != 10) {
-                        sb.append((char) c);
-                    }
-                }
-            } catch (IOException e) {
-                s_logger.error("Cannot read port", e);
-            } finally {
-                try {
-                    m_commIs.close();
-                } catch (IOException e) {
-                    s_logger.error("Cannot close buffered reader", e);
-                }
-            }
-        }
-    }
+						// reset the buffer
+						sb = new StringBuilder();
+					} else if (c != 10) {
+						sb.append((char) c);
+					}
+				}
+			} catch (IOException e) {
+				s_logger.error(APP_ID + "Cannot read port", e);
+			} finally {
+				try {
+					m_commIs.close();
+				} catch (IOException e) {
+					s_logger.error(APP_ID + "Cannot close buffered reader", e);
+				}
+			}
+		}
+	}
 }
